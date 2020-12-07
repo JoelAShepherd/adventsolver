84efe1ef8eb475f9fcba1255ac9d28a3dba730f5
JShep <66128412+JoelAShepherd@users.noreply.github.com>
2020-12-07 21:21:31 +0000

    two days done

--- src/day1/day1log.js
+++ src/day1/day1log.js
@@ -1,6 +1,50 @@
 import React from 'react'
+import input from './input'
 
 function Day1log() {
+    let inputArr = input.split(/\n/)
+    inputArr = inputArr.map(num => parseInt(num))
+    console.log(inputArr)
+
+    let c, d;
+
+    for (let i =0; i<200; i++){
+        let a = inputArr[i];
+        for (let j=1; j<(200-i); j++ ){
+            let b = inputArr[i+j]
+            if (a+b === 2020){
+                c= a;
+                d = b;
+                
+            }
+        }
+    }
+
+    let e = c*d
+
+    console.log('a: ', c, 'b: ', d, 'e: ', e) // first problem
+
+    var ans;
+    var x, y, z;
+
+    for (let k=0; k<200; k++){
+        let f = inputArr[k];
+        for (let l=1; l<(200-k); l++){
+            let g = inputArr[k+l]
+            for (let m=2; m<(200-k-l); m++){
+                let h = inputArr[k+l+m]
+                if (f+g+h === 2020){
+                    x=f;
+                    y=g;
+                    z=h;
+                    
+                }
+            }
+        }
+    }
+    ans = x*y*z
+    console.log(x, y, z, ans)
+
     return (
         <div>
             <h1>Hello! Day1:</h1>
